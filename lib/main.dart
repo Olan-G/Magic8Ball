a570dd8a7b67d1882e690495a9c059174260db1c
Olan Gomez <johnolansaldivar.gomez@bicol-u.edu.ph>
2024-01-26 17:54:06 +0800

    task(main): added scaffold, appbar, and image

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Image.asset('images/ball1.png'),
+      ),
+    );
+  }
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[400],
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Ask Me Anything',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
